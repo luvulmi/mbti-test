d089e024b49e84baefd82a2deb82662226a5a6da
김유림 <yulmi123@minisoft.co.kr>
2025-01-07 10:12:42 +0900

    질문 페이지 디자인 추가가

--- mbti-test-react/src/pages/Question.tsx
+++ mbti-test-react/src/pages/Question.tsx
@@ -1,10 +1,24 @@
 
 
+
+
 const Question = () => {
+  // 답변 클릭 시 문제 받아오기
+
+  // 문제 자르기
+
+
 
   return (
     <div className="container">
-      <h1>Question</h1>
+      {/* 프로그래스바 추후 추가 */}
+      <div className="question-box">
+        <h4>1/12</h4>
+        <h3>문제 1.</h3>
+        <p className="question">ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</p>
+        <button className="answer-btn">답변1</button>
+        <button className="answer-btn">답변2</button>
+      </div>
     </div>
   );
 }
